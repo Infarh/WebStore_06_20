83f506f60c882979d72da0fdbe20390773201a6a
Infarh <shmachilin@yandex.ru>
2020-06-23 20:40:03 +0300

    Отображение списка сотрудников и их карточек

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly List<Employee> __Employees = new List<Employee>
         {
-            return View();
+            new Employee
+            {
+                Id = 1,
+                Surname = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Age = 39
+            },
+            new Employee
+            {
+                Id = 2,
+                Surname = "Петров",
+                Name = "Пётр",
+                Patronymic = "Петрович",
+                Age = 27
+            },
+            new Employee
+            {
+                Id = 3,
+                Surname = "Сидоров",
+                Name = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 18
+            },
+        };
+
+        public IActionResult Index() => View();
+
+        public IActionResult Employees()
+        {
+            ViewBag.Title = "123";
+            ViewData["TestValue"] = "Value - test";
+
+            var employees = __Employees;
+            return View(employees);
+        }
+
+        public IActionResult EmployeeInfo(int id)
+        {
+            var employee = __Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
         }
     }
 }
