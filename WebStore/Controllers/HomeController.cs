f173959a3d1d65ed3c2d1305cca868b4b5d79376
Infarh <shmachilin@yandex.ru>
2020-06-26 20:37:59 +0300

    Заготовки представлений для основных страниц дизайнерского макета

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index() => View();
 
-        
+        public IActionResult Blogs() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
     }
 }
