013190cc8cdf45f493edba8160004a6c66f9187f
Infarh <shmachilin@yandex.ru>
2020-06-30 20:22:18 +0300

    Переносим тестовые данные в отдельное место

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -8,41 +9,13 @@ namespace WebStore.Controllers
     //[Route("Users")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Surname = "Иванов",
-                Name = "Иван",
-                Patronymic = "Иванович",
-                Age = 39
-            },
-            new Employee
-            {
-                Id = 2,
-                Surname = "Петров",
-                Name = "Пётр",
-                Patronymic = "Петрович",
-                Age = 27
-            },
-            new Employee
-            {
-                Id = 3,
-                Surname = "Сидоров",
-                Name = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 18
-            },
-        };
-
         //[Route("All")]
-        public IActionResult Index() => View(__Employees);
+        public IActionResult Index() => View(TestData.Employees);
 
         //[Route("User-{id}")]
         public IActionResult Details(int id)
         {
-            var employee = __Employees.FirstOrDefault(e => e.Id == id);
+            var employee = TestData.Employees.FirstOrDefault(e => e.Id == id);
             if (employee is null)
                 return NotFound();
 
