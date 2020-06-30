47e317f93ea136c936388a4c1921b0fd1bc55d96
Infarh <shmachilin@yandex.ru>
2020-06-30 20:18:53 +0300

    Интерфейс сервиса работы с сотрудниками

---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+
+        Employee GetById(int id);
+
+        int Add(Employee employee);
+
+        void Employee(Employee employee);
+
+        bool Delete(int id);
+
+        void SaveChanges();
+    }
+}
