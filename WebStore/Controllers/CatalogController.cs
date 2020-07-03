3bb800ae72259bec3b929d459f6be6d10dd7e1ab
Infarh <shmachilin@yandex.ru>
2020-07-03 21:17:07 +0300

    Контроллер каталога товаров

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Shop(ProductFilter Filter)
+        {
+            var products = _ProductData.GetProducts(Filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = Filter.SectionId,
+                BrandId = Filter.BrandId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).OrderBy(p => p.Order)
+            });
+        }
+    }
+}
