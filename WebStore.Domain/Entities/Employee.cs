c0e1957fe302a82240a833b60b5a7d86a84103e7
Infarh <shmachilin@yandex.ru>
2020-07-10 19:20:52 +0300

    Перенос сущности Сотрудник в проект доменной области

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace WebStore.Models
+namespace WebStore.Domain.Entities
 {
     public class Employee
     {
--- WebStore/Infrastructure/Mapping/EmployeeMapper.cs
+++ WebStore/Infrastructure/Mapping/EmployeeMapper.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using WebStore.Models;
+using WebStore.Domain.Entities;
 using WebStore.ViewModels;
 
 namespace WebStore.Infrastructure.Mapping
