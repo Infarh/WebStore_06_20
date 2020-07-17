4220606196b6c5c672321a019e67ab9655949e57
Infarh <shmachilin@yandex.ru>
2020-07-17 21:02:10 +0300

    Представления профиля пользвоателя с таблицей заказов

--- WebStore/Controllers/UserProfileController.cs
+++ WebStore/Controllers/UserProfileController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -7,5 +11,19 @@ namespace WebStore.Controllers
     public class UserProfileController : Controller
     {
         public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrders(User.Identity.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+                Id = order.Id,
+                Name = order.Name,
+                Phone = order.Phone,
+                Address = order.Address,
+                TotalSum = order.Items.Sum(item => item.Price * item.Quantity)
+            }));
+        }
     }
 }
