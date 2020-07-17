90770b997af5391f9afac85660c7e4a347e56f56
Infarh <shmachilin@yandex.ru>
2020-07-17 19:18:11 +0300

    Ограничение доступа к области администратора

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
