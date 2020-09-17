82edafe98b60ab3c647f3201985051837e3f0225
Infarh <shmachilin@yandex.ru>
2020-09-17 21:19:56 +0300

    Тестирование WebAPI

---
+++ UI/WebStore/Controllers/WebAPITestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestApi;
+
+namespace WebStore.Controllers
+{
+    public class WebAPITestController : Controller
+    {
+        private readonly IValueService _ValueService;
+
+        public WebAPITestController(IValueService ValueService) => _ValueService = ValueService;
+
+        public IActionResult Index()
+        {
+            var values = _ValueService.Get();
+
+            return View(values);
+        }
+    }
+}
