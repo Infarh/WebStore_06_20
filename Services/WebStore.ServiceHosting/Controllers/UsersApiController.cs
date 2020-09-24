826e5f996ec6a3ef5c3d819035dc17a0c9fe9adf
Infarh <shmachilin@yandex.ru>
2020-09-24 19:30:20 +0300

    Методы WebAPI, позволяющие вернуть всех пользователей и все роли из БД

--- Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebStore.DAL.Context;
 using WebStore.Domain;
 using WebStore.Domain.Entities.Identity;
@@ -16,5 +19,9 @@ namespace WebStore.ServiceHosting.Controllers
         {
             _UserStore = new UserStore<User, Role, WebStoreDB>(db);
         }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await _UserStore.Users.ToArrayAsync();
+
     }
 }
