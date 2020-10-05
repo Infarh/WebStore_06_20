118b1b8479108382717f61b312de98bec1475f59
Infarh <shmachilin@yandex.ru>
2020-10-05 19:30:14 +0300

    Заготовка таг-хелпера и проверка его функционирования

---
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(/*"Имя тега", */ Attributes = AttributeName)]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        public const string AttributeName = "is-active-route";
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+
+            output.Attributes.RemoveAll(AttributeName);
+        }
+    }
+}
